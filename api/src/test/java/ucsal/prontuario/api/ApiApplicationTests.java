5fb7838671a6f995f3865b6dbe741651635f9b47
Caio Nunes <caionunes.profissional@gmail.com>
2023-09-10 16:28:15 -0300

    Adicionando pasta do frontend e iniciando as configurações iniciais da API

---
+++ api/src/test/java/ucsal/prontuario/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ucsal.prontuario.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
