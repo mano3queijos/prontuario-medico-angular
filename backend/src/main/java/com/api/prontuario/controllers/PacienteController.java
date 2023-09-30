d1354b6a6a5838000e83fe687415176a96704237
Caio Nunes <caionunes.profissional@gmail.com>
2023-09-30 18:09:07 -0300

    Adicionando o BuscarTodos e BuscarPorId ao Paciente Controller

--- backend/src/main/java/com/api/prontuario/controllers/PacienteController.java
+++ backend/src/main/java/com/api/prontuario/controllers/PacienteController.java
@@ -2,22 +2,42 @@ package com.api.prontuario.controllers;
 
 import com.api.prontuario.dtos.PacienteDto;
 import com.api.prontuario.entites.Paciente;
+import com.api.prontuario.services.PacienteService;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/paciente")
 public class PacienteController {
 
+    private final PacienteService pacienteService;
 
-    @GetMapping("/paciente")
-    public ResponseEntity<PacienteDto> CadastrarPaciente(@RequestBody @Valid PacienteDto pacienteDto) {
-        Paciente paciente = new Paciente(pacienteDto);
-        return new ResponseEntity<>(new PacienteDto(paciente), HttpStatus.CREATED);
+    public PacienteController(PacienteService pacienteService) {
+        this.pacienteService = pacienteService;
     }
+
+    @GetMapping
+    public ResponseEntity<Page<PacienteDto>> buscarTodos (@PageableDefault(size = 10) Pageable paginacao) {
+        var page = pacienteService.listarTodos(paginacao).map(PacienteDto::new);
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PacienteDto> buscarPorId (@PathVariable Long id) {
+        Paciente paciente = pacienteService.buscarPorId(id);
+        return ResponseEntity.ok(new PacienteDto(paciente));
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<PacienteDto> cadastrarPaciente(@RequestBody @Valid PacienteDto pacienteDto) {
+        Paciente paciente = pacienteService.register(new Paciente(pacienteDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new PacienteDto(paciente));
+    }
+
+
 }
