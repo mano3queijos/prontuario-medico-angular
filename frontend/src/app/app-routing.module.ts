158266326c8db852bef1b700a952f85954436ae6
Emanuel <101946589+mano3queijos@users.noreply.github.com>
2023-10-30 16:42:50 -0300

    navbar ok

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{path: '', pathMatch: 'full', redirectTo: 'login'},  {
-    path: 'pacientes',
-    loadChildren: () => import('./pacientes/pacientes.module').then(m => m.PacientesModule)
-  }
+const routes: Routes = [{ path: '', pathMatch: 'full', redirectTo: 'login' }, {
+  path: 'pacientes',
+  loadChildren: () => import('./pacientes/pacientes.module').then(m => m.PacientesModule)
+},
+{ path: 'home', component: HomeComponent },
+
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,9 +12,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, NavbarComponent],
+  declarations: [AppComponent, LoginComponent, NavbarComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
