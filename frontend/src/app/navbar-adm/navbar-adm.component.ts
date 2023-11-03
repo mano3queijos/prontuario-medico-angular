9a05852b1bc8b1e62000ee63d78f298f8d4b8d12
Emanuel <101946589+mano3queijos@users.noreply.github.com>
2023-11-03 14:22:28 -0300

    home works

--- frontend/src/app/navbar-adm/navbar-adm.component.ts
+++ frontend/src/app/navbar-adm/navbar-adm.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AxiosService } from '../axios.service';
 
 @Component({
   selector: 'app-navbar-adm',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar-adm.component.scss']
 })
 export class NavbarAdmComponent {
+  activeCollapse: string | null = null; // Define null para nenhum colapso ativo por padrão
+
+  collapses: { name: string; isCollapsed: boolean }[] = [
+    { name: 'paginaInicial', isCollapsed: true },
+    { name: 'Medicos', isCollapsed: true },
+    { name: 'Adms', isCollapsed: true },
+    // Adicione mais colapsos conforme necessário
+  ];
+  constructor(private AxiosService: AxiosService) { }
+
+  toggleCollapse(collapseName: string) {
+    if (this.activeCollapse === collapseName) {
+      this.activeCollapse = null; // Fecha o colapso se ele estiver aberto
+    } else {
+      this.activeCollapse = collapseName; // Abre o colapso clicado
+    }
+  }
+
+  isActive(collapseName: string): boolean {
+    return this.activeCollapse === collapseName;
+  }
+
+  logout(): void {
+    this.AxiosService.logout();
+  }
+
+  isAuthenticated(): boolean {
+    return this.AxiosService.isAuthenticated();
+  }
 
 }
